da2b041b02c42f71ac3b782feb0886f31619c32b
anupama <anupamagadde@yahoo.com>
2020-09-10 23:45:52 -0500

    add show action to gram_positives_controller

--- MicrobiologyApp/app/controllers/gram_positives_controller.rb
+++ MicrobiologyApp/app/controllers/gram_positives_controller.rb
@@ -1,2 +1,10 @@
 class GramPositivesController < ApplicationController
+
+    def index
+        @gram_positives = GramPositive.all
+    end
+
+    def show
+        @gram_positive = GramPositive.find_by(id: params[:id])
+    end
 end
